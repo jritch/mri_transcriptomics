26ba98059436b48befe2af89abef495ce4ae97f4
jritch <jacob.w.k.ritchie@gmail.com>
2017-02-14 00:28:54 -0500

    Update single gene to filter by region (in this case cortex)

--- single_gene.py
+++ single_gene.py
@@ -44,6 +44,8 @@ def get_single_gene_data(gene_exp_fh,gene_name,indices=None):
 def main():
   gene_name =  "CAPN6"
   MRI_dimension = 2 # 0: T1, 1: T2, 2: ratio
+  regionID = 4008 #cortex
+  region_name = "cortex"
 
   files = ["10021.matrix.regionID.MRI(xyz).29131 x 893.txt",
            "12876.matrix.regionID.MRI(xyz).29131 x 363.txt",
@@ -57,33 +59,40 @@ def main():
   #header = "\"(x,y,z)\"," + ",".join([ '"' + f + "_MRI\",\"" + f + "_" + gene_name + '"'  for f in brain_ids])
   header = "\"(x,y,z)\"," + ",".join([ '"' + f + "_MRI\",\"" + f + "_" + gene_name + '"'  for f in brain_ids])
   baseProjectFolder = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/" 
-  filenames = [baseProjectFolder + "single_gene_data/" + f + "." + gene_name + ".MRI(xyz).expression.txt" for f in files]
+  filenames = [baseProjectFolder + "single_gene_data/" + f + "." + gene_name + "." + region_name + ".MRI(xyz).expression.csv" for f in brain_ids]
   print filenames
   
+
+  o = analysis.Ontology(config.ontologyFolder + "Ontology.csv")
+
   num_brains = len(brain_ids)
   for i in range(num_brains):
 
         gene_exp_fh = open(os.path.join(config.MRIFolder,files[i]))
         coords,coord_to_region_map = analysis.get_coords_and_region_ids_from_gene_exp_data(gene_exp_fh)
-        single_gene_data = np.array(get_single_gene_data(gene_exp_fh,gene_name))
+        indices = analysis.get_flat_coords_from_region_id(regionID,coords,coord_to_region_map,o)
+        single_gene_data = np.array(get_single_gene_data(gene_exp_fh,gene_name,indices))
         gene_exp_fh.close()
+
 
         mri_data = analysis.load_nifti_data(config.baseAllenFolder + "normalized_microarray_donor" + brain_ids[i])[MRI_dimension]
         flat_mri_data = np.array(analysis.flatten_mri_data(mri_data,coords))
+        region_specific_flat_mri_data = [flat_mri_data[j] for j in indices]
 
-        
-        results = np.zeros((3,len(coords)))
+        results = np.zeros((3,len(indices)))
 
-        results[1,:] = flat_mri_data
+        results[1,:] = region_specific_flat_mri_data
         results[2,:] = single_gene_data
 
-
         print os.getcwd()
+
+        coord_subset = [coords[j] for j in indices]
+
         with open(filenames[i], "w") as f:
           f.write("\"(x,y,z)\",MRI_Intensity," + gene_name  + "\"")
-          for j in range(len(coords)):
+          for j in range(len(indices)):
 
-            str_coords = [str(coord) for coord in coords[j]]
+            str_coords = [str(coord) for coord in coord_subset[j]]
             coord_string = "(" + ",".join(str_coords) + ")" 
             f.write("\"" + coord_string + "\","  + str(results[1,j]) + "," + str(results[2,j]) + "\n")
 
