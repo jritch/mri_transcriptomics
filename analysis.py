1e969ee5b3b9dc0cd062aa64fd7d508ae8cbc7a7
Jacob Ritchie <jacob.w.k.ritchie@gmail.com>
2017-05-08 14:43:47 -0400

    refactor ontology into its own file

--- analysis.py
+++ analysis.py
@@ -15,6 +15,8 @@ from scipy.stats import pearsonr
 from scipy.stats import spearmanr
 import numpy as np
 
+from ontology import *
+
 
 print inspect.getfile(inspect.currentframe()) # script filename (usually with path)
 baseProjectFolder = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/" # script directory
@@ -25,51 +27,6 @@ print "Base script folder:" + baseProjectFolder
 
 R_VALUE_FUNCTION = spearmanr
 
-class ClassName(object):
-  """docstring for ClassName"""
-  def __init__(self, arg):
-    super(ClassName, self).__init__()
-    self.arg = arg
-    
-class Ontology(object):
-  """An Ontology has two member variables:
-
-  Attributes:
-    names (dict): 
-      An (ID,name) dictionary 
-    hierarchy (dict):
-      a (parentID,[childID]) dictionary 
-  """
-  def __init__(self,ontology_file_name):
-      names = dict()
-      hierarchy = dict()
-      with open(ontology_file_name) as f:
-        rdr = csv.reader(f)
-        header = next(rdr)
-        for row in rdr:
-          ID = int(row[0])
-          names[ID] = row[2]
-          if row[3]:
-            parentID = int(row[3])
-            if hierarchy.get(parentID):
-                hierarchy[parentID].append(ID)
-            else:
-              hierarchy[parentID] = [ID]
-
-      self.names = names
-      self.hierarchy = hierarchy
-
-  def get_all_regionIDs(self,regionID):
-    IDs = [regionID]
-    all_IDs = []
-    while IDs:
-      # print 'IDs is',IDs
-      ID = IDs.pop()
-      all_IDs.append(ID)
-      child_IDs = self.hierarchy.get(ID)
-      if child_IDs:
-        IDs += child_IDs
-    return all_IDs
 
 def transform_image(data):
   '''
---
+++ ontology.py
@@ -0,0 +1,39 @@
+class Ontology(object):
+  """An Ontology has two member variables:
+
+  Attributes:
+    names (dict): 
+      An (ID,name) dictionary 
+    hierarchy (dict):
+      a (parentID,[childID]) dictionary 
+  """
+  def __init__(self,ontology_file_name):
+      names = dict()
+      hierarchy = dict()
+      with open(ontology_file_name) as f:
+        rdr = csv.reader(f)
+        header = next(rdr)
+        for row in rdr:
+          ID = int(row[0])
+          names[ID] = row[2]
+          if row[3]:
+            parentID = int(row[3])
+            if hierarchy.get(parentID):
+                hierarchy[parentID].append(ID)
+            else:
+              hierarchy[parentID] = [ID]
+
+      self.names = names
+      self.hierarchy = hierarchy
+
+  def get_all_regionIDs(self,regionID):
+    IDs = [regionID]
+    all_IDs = []
+    while IDs:
+      # print 'IDs is',IDs
+      ID = IDs.pop()
+      all_IDs.append(ID)
+      child_IDs = self.hierarchy.get(ID)
+      if child_IDs:
+        IDs += child_IDs
+    return all_IDs
