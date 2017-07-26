2f89eb2a9ab1f6898bed787dc76be9aa56c79b88
Jacob Ritchie <jacob.w.k.ritchie@gmail.com>
2017-07-26 17:24:05 -0400

    fix bug in helper function

--- R Code/Utils.R
+++ R Code/Utils.R
@@ -1,14 +1,15 @@
-writeTableWrapper <- function (table,dir,filename,col_names=NULL,rows=NULL,col_indices=NULL) {
-  if (rows){
-    table = head(table,n=rows)
+writeTableWrapper <- function (my_table,dir,filename,col_names=NULL,rows=NULL,col_indices=NULL) {
+  if (length(rows) > 0){
+    my_table = head(my_table,n=rows)
   }
-  if (col_indices) {
-    table=table[col_indices]
+  if (length(col_indices) > 0) {
+    my_table=my_table[col_indices]
   }
   if (length(col_names) > 0) {
-   names(table) <- col_names
-   write.csv(table, file.path(dir,filename))
+   names(my_table) <- col_names
+   write.csv(my_table, file.path(dir,filename))
   }
 
-  write.csv(table, file.path(dir,filename))
+  write.csv(my_table, file.path(dir,filename))
+  return(my_table)
 }
